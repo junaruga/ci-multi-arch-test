3bb3b556f2360fafe99401abad2b80f6bb3d108e
Jun Aruga <jaruga@redhat.com>
2019-09-15 16:58:33 +0200

    Add rhel 8.0 aarch64, ppc64le and s390x cases.

---
+++ script/print-arch-image-digest.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -xeo pipefail
+
+BASE_IMAGE=${BASE_IMAGE:-registry.access.redhat.com/ubi8}
+INSPECTED_ARCH=${INSPECTED_ARCH:-arm64}
+
+if ! command -v skopeo > /dev/null; then
+    echo "skopeo is required." 1>&2
+    exit 1
+fi
+if ! command -v jq > /dev/null; then
+    echo "jq is required." 1>&2
+    exit 1
+fi
+
+archs=$(skopeo inspect --raw "docker://${BASE_IMAGE}" | \
+    jq -r '.manifests | .[] | .platform.architecture')
+count=0
+for arch in ${archs}; do
+    if [ "${arch}" = "${INSPECTED_ARCH}" ]; then
+
+        break
+    fi
+    count=$(($count + 1))
+done
+
+skopeo inspect --raw "docker://${BASE_IMAGE}" | \
+    jq -r ".manifests | .[$count] | .digest"
