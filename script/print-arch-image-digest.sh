c361b84c74e99d0c9d63a3e7a95c0efd87114162
Jun Aruga <junaruga@users.noreply.github.com>
2019-09-15 19:25:48 +0200

    Refactoring for rhel cases. (#39)

--- script/print-arch-image-digest.sh
+++ script/print-arch-image-digest.sh
@@ -16,13 +16,18 @@ fi
 archs=$(skopeo inspect --raw "docker://${BASE_IMAGE}" | \
     jq -r '.manifests | .[] | .platform.architecture')
 count=0
+found=0
 for arch in ${archs}; do
     if [ "${arch}" = "${INSPECTED_ARCH}" ]; then
-
+        found=1
         break
     fi
     count=$(($count + 1))
 done
+if [ "${found}" != 1 ]; then
+    echo "INSPECTED_ARCH: ${INSPECTED_ARCH} not found." 1>&2
+    exit 1
+fi
 
 skopeo inspect --raw "docker://${BASE_IMAGE}" | \
     jq -r ".manifests | .[$count] | .digest"
