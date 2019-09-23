5dcc04bf6c3273bc2db88578176374244b368b3a
Jun Aruga <junaruga@users.noreply.github.com>
2019-09-23 11:58:14 +0200

    Update a script to print manifest digest with arguments. (#40)
    Rename print-arch-image-digest.sh to print-manifest-arch-digest.sh .

--- script/print-manifest-arch-digest.sh
+++ script/print-manifest-arch-digest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -xeo pipefail
 
-BASE_IMAGE=${BASE_IMAGE:-registry.access.redhat.com/ubi8}
-INSPECTED_ARCH=${INSPECTED_ARCH:-arm64}
+BASE_IMAGE=${1:-registry.access.redhat.com/ubi8}
+MANIFEST_ARCH=${2:-arm64}
 
 if ! command -v skopeo > /dev/null; then
     echo "skopeo is required." 1>&2
@@ -18,14 +18,14 @@ archs=$(skopeo inspect --raw "docker://${BASE_IMAGE}" | \
 count=0
 found=0
 for arch in ${archs}; do
-    if [ "${arch}" = "${INSPECTED_ARCH}" ]; then
+    if [ "${arch}" = "${MANIFEST_ARCH}" ]; then
         found=1
         break
     fi
     count=$(($count + 1))
 done
 if [ "${found}" != 1 ]; then
-    echo "INSPECTED_ARCH: ${INSPECTED_ARCH} not found." 1>&2
+    echo "MANIFEST_ARCH: ${MANIFEST_ARCH} not found." 1>&2
     exit 1
 fi
 
