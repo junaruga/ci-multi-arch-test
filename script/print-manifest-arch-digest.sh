acbfc8bb4bcd26316f3df5b80689786dc7873d8a
David Gräff <davgraeff@gmail.com>
2019-11-27 19:08:55 +0100

    Simplified print-manifest-arch-digest.sh (#46)
    Signed-of-by: David Graeff <david.graeff@web.de>

--- script/print-manifest-arch-digest.sh
+++ script/print-manifest-arch-digest.sh
@@ -13,21 +13,4 @@ if ! command -v jq > /dev/null; then
     exit 1
 fi
 
-archs=$(skopeo inspect --raw "docker://${BASE_IMAGE}" | \
-    jq -r '.manifests | .[] | .platform.architecture')
-count=0
-found=0
-for arch in ${archs}; do
-    if [ "${arch}" = "${MANIFEST_ARCH}" ]; then
-        found=1
-        break
-    fi
-    count=$(($count + 1))
-done
-if [ "${found}" != 1 ]; then
-    echo "MANIFEST_ARCH: ${MANIFEST_ARCH} not found." 1>&2
-    exit 1
-fi
-
-skopeo inspect --raw "docker://${BASE_IMAGE}" | \
-    jq -r ".manifests | .[$count] | .digest"
+skopeo inspect --raw "docker://${BASE_IMAGE}" | jq -r ".manifests[] | select(.platform.architecture == \"${MANIFEST_ARCH}\") | .digest"
